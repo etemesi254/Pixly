ad56a8e6132134e2f4b6447cdbb790106c01eca6
caleb <etemesicaleb@gmail.com>
2023-12-06 23:47:16 +0300

    main/components/ImageInformation: Move image information into separate components from main

---
+++ src/main/kotlin/components/ImageInformation.kt
@@ -0,0 +1,80 @@
+package components
+
+import AppContext
+import ZilImageAndBitmapInterop
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Divider
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import java.io.File
+import java.text.DecimalFormat
+
+@Composable
+fun ImageInformationComponent(appCtx: AppContext, file:File){
+    val innerImage = appCtx.image.inner;
+
+    Box(modifier = Modifier.padding(10.dp)) {
+        CollapsibleBox("Information", appCtx.showStates.showInformation, {
+            appCtx.showStates.showInformation =
+                appCtx.showStates.showInformation.xor(true);
+        }) {
+            Column(modifier = Modifier.padding(10.dp)) {
+                Row(
+                    modifier = Modifier.fillMaxWidth().padding(10.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text("Width")
+                    Text(innerImage.width().toString() + " px");
+                }
+                Divider()
+                Row(
+                    modifier = Modifier.fillMaxWidth().padding(10.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text("Height")
+                    Text(innerImage.height().toString() + " px");
+                }
+
+                Divider()
+                Row(
+                    modifier = Modifier.fillMaxWidth().padding(10.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text("Colorspace")
+                    Text(innerImage.colorspace().toString());
+                }
+                Divider()
+                Row(
+                    modifier = Modifier.fillMaxWidth().padding(10.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text("Size")
+                    Text(formatSize(file.length()));
+                }
+            }
+        }
+    }
+
+
+}
+
+
+fun formatSize(bytes: Long): String {
+    var bytes = bytes.toDouble();
+    val formatter = DecimalFormat("####.##")
+    if (bytes < 1024.0) {
+        return formatter.format(bytes) + " B"
+    }
+    bytes /= 1024.0;
+    if (bytes < 1024.0) {
+        return formatter.format(bytes) + " KB"
+    }
+    bytes /= 1024.0;
+    if (bytes < 1024.0) {
+        return formatter.format(bytes) + " MB"
+    }
+    bytes /= 1024.0;
+    return formatter.format(bytes) + " GB"
+}
