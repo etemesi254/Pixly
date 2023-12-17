7b57c6a2e1cb75e3e2ddb8a7794098e4f8943469
caleb <etemesicaleb@gmail.com>
2023-12-17 21:35:51 +0300

    utils: Actually detect all types of images

--- src/main/kotlin/utils.kt
+++ src/main/kotlin/utils.kt
@@ -25,39 +25,41 @@ fun isImage(file: File): Boolean {
 
 @OptIn(ExperimentalUnsignedTypes::class)
 fun imageFormat(byteArray: UByteArray): ZilImageFormat {
+
+
     if (byteArray.slice(0..1) == ubyteArrayOf(0xFFu, 0xD8u).asList()) {
         return ZilImageFormat.JPEG;
     } else if (byteArray.slice(0..7) == ubyteArrayOf(137u, 80u, 78u, 71u, 13u, 10u, 26u, 10u).asList()) {
         return ZilImageFormat.PNG
-    } else if (byteArray.slice(0..1) == "P5".toList().map { it -> it.code.toByte() }.toList()) {
+    } else if (byteArray.slice(0..1) == "P5".toList().map { it -> it.code.toUByte() }.toList()) {
         return ZilImageFormat.PPM
-    } else if (byteArray.slice(0..1) == "P6".toList().map { it -> it.code.toByte() }.toList()) {
+    } else if (byteArray.slice(0..1) == "P6".toList().map { it -> it.code.toUByte() }.toList()) {
         return ZilImageFormat.PPM
-    } else if (byteArray.slice(0..1) == "P7".toList().map { it -> it.code.toByte() }.toList()) {
+    } else if (byteArray.slice(0..1) == "P7".toList().map { it -> it.code.toUByte() }.toList()) {
         return ZilImageFormat.PPM
-    } else if (byteArray.slice(0..1) == "Pf".toList().map { it -> it.code.toByte() }.toList()) {
+    } else if (byteArray.slice(0..1) == "Pf".toList().map { it -> it.code.toUByte() }.toList()) {
         return ZilImageFormat.PPM
-    } else if (byteArray.slice(0..1) == "PF".toList().map { it -> it.code.toByte() }.toList()) {
+    } else if (byteArray.slice(0..1) == "PF".toList().map { it -> it.code.toUByte() }.toList()) {
         return ZilImageFormat.PPM
-    } else if (byteArray.slice(0..3) == "8BPS".toList().map { it -> it.code.toByte() }.toList()) {
+    } else if (byteArray.slice(0..3) == "8BPS".toList().map { it -> it.code.toUByte() }.toList()) {
         return ZilImageFormat.PSD
-    } else if (byteArray.slice(0..7) == "farbfeld".toList().map { it -> it.code.toByte() }.toList()) {
+    } else if (byteArray.slice(0..7) == "farbfeld".toList().map { it -> it.code.toUByte() }.toList()) {
         return ZilImageFormat.PSD
-    } else if (byteArray.slice(0..3) == "8BPS".toList().map { it -> it.code.toByte() }.toList()) {
+    } else if (byteArray.slice(0..3) == "8BPS".toList().map { it -> it.code.toUByte() }.toList()) {
         return ZilImageFormat.PSD
-    } else if (byteArray.slice(0..3) == "qoif".toList().map { it -> it.code.toByte() }.toList()) {
+    } else if (byteArray.slice(0..3) == "qoif".toList().map { it -> it.code.toUByte() }.toList()) {
         return ZilImageFormat.PSD
-    } else if (byteArray.slice(0..10) == "#?RADIANCE\n".toList().map { it -> it.code.toByte() }.toList()) {
+    } else if (byteArray.slice(0..10) == "#?RADIANCE\n".toList().map { it -> it.code.toUByte() }.toList()) {
         return ZilImageFormat.HDR
-    } else if (byteArray.slice(0..6) == "#?RGBE\n".toList().map { it -> it.code.toByte() }.toList()) {
+    } else if (byteArray.slice(0..6) == "#?RGBE\n".toList().map { it -> it.code.toUByte() }.toList()) {
         return ZilImageFormat.HDR
-    } else if (byteArray.slice(0..11) == arrayOf(
-            0x00, 0x00, 0x00, 0x0C, 0x4A, 0x58, 0x4C, 0x20, 0x0D, 0x0A, 0x87, 0x0A
-        )
+    } else if (byteArray.slice(0..11) == ubyteArrayOf(
+            0x00u, 0x00u, 0x00u, 0x0Cu, 0x4Au, 0x58u, 0x4Cu, 0x20u, 0x0Du, 0x0Au, 0x87u, 0x0Au
+        ).toList()
     ) {
         return ZilImageFormat.JPEG_XL
     }
-    else if (byteArray.slice(0..1)== arrayOf(0xFF,0x0A)){
+    else if (byteArray.slice(0..1)== ubyteArrayOf(0xFFu,0x0Au).toList()){
         return ZilImageFormat.JPEG_XL
     }
 
