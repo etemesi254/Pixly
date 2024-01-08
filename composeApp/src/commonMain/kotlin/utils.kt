748ec559eedad4dba2a74473945ca2e27f918dee
caleb <etemesicaleb@gmail.com>
2024-01-08 14:33:04 +0300

    common: Move `calResize` to common from desktopUtils.kt

--- composeApp/src/commonMain/kotlin/utils.kt
+++ composeApp/src/commonMain/kotlin/utils.kt
@@ -84,3 +84,23 @@ fun fillPaths(appContext: AppContext, files: List<File>) {
         i += 1
     }
 }
+
+
+
+fun calcResize(image: ZilBitmapInterface, newW: Long, newH: Long): List<Long> {
+    val oldW = image.innerInterface().width().toFloat()
+    val oldH = image.innerInterface().height().toFloat()
+
+    val ratioW = oldW / newW.toFloat()
+    val ratioH = oldH / newH.toFloat()
+
+    val percent = if (ratioH < ratioW) {
+        ratioW
+    } else {
+        ratioH
+    };
+    val t = (oldW / percent).toLong()
+    val u = (oldH / percent).toLong()
+    return listOf(t, u)
+
+}
