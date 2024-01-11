ac0cc9302848b53cc39f36887ae38ad5302be59f
caleb <etemesicaleb@gmail.com>
2024-01-11 13:44:07 +0300

    android/common: Improve bottom icons

--- composeApp/src/androidMain/kotlin/org/cae/pixly/ImageFiltersComponent.kt
+++ composeApp/src/androidMain/kotlin/org/cae/pixly/ImageFiltersComponent.kt
@@ -9,6 +9,10 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import extensions.launchOnIoThread
 import imageops.imageBrighten
@@ -20,13 +24,15 @@ enum class ImageFiltersComponentClicked {
     None,
     Light,
     HSL,
-    Blur
+    Blur,
+    Orientation,
 }
 
 @Composable
 fun LightFilters(appContext: AppContext) {
     val scope = rememberCoroutineScope();
-    Box(modifier = Modifier.height(200.dp).fillMaxWidth()
+    Box(
+        modifier = Modifier.height(200.dp).fillMaxWidth()
     ) {
         Column(modifier = Modifier.verticalScroll(rememberScrollState()).padding(vertical = 10.dp)) {
             Box(
@@ -100,30 +106,66 @@ fun ImageFilters(appContext: AppContext) {
             ImageFiltersComponentClicked.Light -> LightFilters(appContext)
             ImageFiltersComponentClicked.HSL -> LightFilters(appContext)
             ImageFiltersComponentClicked.Blur -> LightFilters(appContext)
+            ImageFiltersComponentClicked.Orientation -> AndroidOrientationFilters(appContext)
         }
         Divider(modifier = Modifier.fillMaxWidth())
-        Row() {
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 10.dp, vertical = 0.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
             IconButton(onClick = {
                 imageFiltersComponentClicked = ImageFiltersComponentClicked.Light
             },
                 modifier = Modifier.backgroundColorIfCondition(MaterialTheme.colors.primary) {
                     imageFiltersComponentClicked == ImageFiltersComponentClicked.Light
                 }) {
-                Text("Light")
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Icon(
+                        painterResource(R.drawable.light_bulb_svgrepo_com),
+                        contentDescription = "Light Icon",
+                        modifier = Modifier.size(25.dp)
+                    )
+                    Spacer(modifier = Modifier.height(5.dp))
+                    Text("Light", textAlign = TextAlign.Center, fontSize = TextUnit(12F, TextUnitType.Sp))
+                }
             }
             IconButton(onClick = {
-                imageFiltersComponentClicked = ImageFiltersComponentClicked.HSL
+                imageFiltersComponentClicked = ImageFiltersComponentClicked.Orientation
             }, modifier = Modifier.backgroundColorIfCondition(MaterialTheme.colors.primary) {
-                imageFiltersComponentClicked == ImageFiltersComponentClicked.HSL
+                imageFiltersComponentClicked == ImageFiltersComponentClicked.Orientation
             }) {
-                Text("HSL")
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Icon(
+                        painterResource(R.drawable.orientation_svgrepo_com),
+                        contentDescription = "Orientation Icon",
+                        modifier = Modifier.size(25.dp)
+                    )
+                    Spacer(modifier = Modifier.height(5.dp))
+
+                    Text(
+                        "Orientation",
+                        textAlign = TextAlign.Center,
+                        fontSize = TextUnit(12F, TextUnitType.Sp)
+                    )
+                }
+
             }
+
             IconButton(onClick = {
                 imageFiltersComponentClicked = ImageFiltersComponentClicked.Blur
             }, modifier = Modifier.backgroundColorIfCondition(MaterialTheme.colors.primary) {
                 imageFiltersComponentClicked == ImageFiltersComponentClicked.Blur
             }) {
-                Text("Blur")
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Icon(
+                        painterResource(R.drawable.blur_svgrepo_com),
+                        contentDescription = "Blur Icon",
+                        modifier = Modifier.size(25.dp)
+                    )
+                    Spacer(modifier = Modifier.height(5.dp))
+
+                    Text("Blur", fontSize = TextUnit(12F, TextUnitType.Sp))
+                }
             }
         }
     }
