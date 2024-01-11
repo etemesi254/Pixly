0dc1509872906dc0185f9a4548b521d7b14bdaf6
caleb <etemesicaleb@gmail.com>
2024-01-11 17:12:11 +0300

    android: Remove material3 from library

--- composeApp/src/androidMain/kotlin/org/cae/pixly/BottomBar.kt
+++ composeApp/src/androidMain/kotlin/org/cae/pixly/BottomBar.kt
@@ -6,8 +6,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -37,7 +35,7 @@ fun InformationPanel(appCtx: AppContext) {
     }
 }
 
-@OptIn(ExperimentalResourceApi::class, ExperimentalMaterialApi::class, ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalResourceApi::class, ExperimentalMaterialApi::class)
 @Composable
 fun BottomBar(appCtx: AppContext) {
     val scope = rememberCoroutineScope()
